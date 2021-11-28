b9e2cfcec51e9b744e76b71062b23f0bf4ab235b
Ektora <zsoltlucski@gmail.com>
2021-11-28 12:15:21 +0100

    Internet kapcsolat hiánya megoldva

--- covid19.js
+++ covid19.js
@@ -1,17 +1,37 @@
 export const loadData = async (country) => {
+    try{
     const esetek = getCases(country);
     const oltottak = getVaccines(country);
+    }catch (e){
+        console.error("Hiba történt! ",e);
+        throw e;
+    }
 }
 
 const getCases = async (country) => {
+    if(!navigator.onLine){
+        throw 'Jeleneleg nincs hálózati kapcsolat!';
+    }
     const response = await fetch(`https://covid-api.mmediagroup.fr/v1/cases?country=${country}`);
-    const jsonResponse = await response.json(); 
+    
+    
+    if (!response.ok) {
+        console.log(response.status);
+        throw  'Error loading getCases';
+    }
+    const jsonResponse = await response.json();
     return jsonResponse;
     
 }
 
 const getVaccines = async (country) => {
+    if(!navigator.onLine){
+        throw 'Jeleneleg nincs hálózati kapcsolat!';
+    }
     const response = await fetch(`https://covid-api.mmediagroup.fr/v1/vaccines?country=${country}`);
+    if(!response.ok){
+        throw 'Hiba az oltási adatok elérése közben!';
+    }
     const jsonResponse = await response.json(); 
     return jsonResponse;
     
